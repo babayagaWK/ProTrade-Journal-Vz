6ceb8b2b5a9b6b60fbefba5e081a002ce600c9ff
Your Name <your-email@example.com>
2025-11-25 18:25:11 +0700

    chore: Add Android build options to Capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,11 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  android: {
+    buildOptions: {
+      signingConfig: 'debug'
+    }
   }
 };
 
